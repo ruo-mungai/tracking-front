57514cb3febd4517582ad97247e0dfea780f9ad9
Yahya <yahyahussein670@gmail.com>
2022-10-27 20:47:41 +0300

    Add the titles and icons of the content displayed in the sidebar

---
+++ src/SideBarData.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+
+export const SideBarData = [
+    {
+        title: "Projects",
+        icon: <HomeIcon />,
+        path: "/h"
+    },
+    {
+        title: "Add Projects",
+        icon: <PostAddIcon />,
+        path: "/addproject"
+    },
+    {
+        title: "Logout",
+        icon: <LogoutIcon />,
+        path: "/logout"
+    }
+];
