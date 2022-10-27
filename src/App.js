2a06e09ac23341bf8f00470cec75101c59f2c157
Yahya <yahyahussein670@gmail.com>
2022-10-27 17:02:14 +0300

    Add sidebar component as a child, add route path to sidebar's children

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Sidebar from './components/Sidebar';
+import ProjectList from './pages/ProjectList'
+import AddProjects from './pages/AddProjects'
+import LogOut from './pages/LogOut'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar>
+    <Routes>
+        <Route path="/h" element={<ProjectList/>} />
+        <Route path="/addproject" element={<AddProjects/>} />
+        <Route path="/logout" element={<LogOut/>} />
+    </Routes>
+    </Sidebar>
+    </>
   );
 }
 
