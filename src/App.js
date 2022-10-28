d3e652f1e4b26716ca0845e937c6eb635cad63f7
Patrick <ruomungai@gmail.com>
2022-10-28 13:24:42 +0300

    Edit project debug

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Sidebar from './components/Sidebar';
 import AddProjects from './pages/AddProjects'
 import LogOut from './pages/LogOut'
 import AllProjects from './pages/AllProjects';
+import EditProject from "./pages/EditProject";
 
 
 function App() {
   return (
     <>
-    <Sidebar>
-    <Routes>
-        <Route path="/" element={<AllProjects/>} />
-        <Route path="/addproject" element={<AddProjects/>} />
-        <Route path="/logout" element={<LogOut/>} />
-    </Routes>
-    </Sidebar>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<AllProjects />} />
+          <Route path="/addproject" element={<AddProjects />} />
+          <Route path="/edit/:id" element={<EditProject />} />
+          <Route path="/logout" element={<LogOut />} />
+        </Routes>
+      </Sidebar>
     </>
   );
 }
--- src/pages/SideBarData.js
+++ src/pages/SideBarData.js
@@ -2,11 +2,17 @@ import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PostAddIcon from '@mui/icons-material/PostAdd';
+import ArticleIcon from "@mui/icons-material/Article";
 
 export const SideBarData = [
     {
         title: "Projects",
         icon: <HomeIcon />,
+        path: "/"
+    },
+    {
+        title: "My Projects",
+        icon: <ArticleIcon />,
         path: "/projects"
     },
     {
