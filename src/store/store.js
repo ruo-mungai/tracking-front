b21dd2063a6777f77a6661d0c74ee17f03e57895
Patrick <ruomungai@gmail.com>
2022-10-28 10:29:50 +0300

    diplay all the projects

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import projectReducer from "../projects/projectslice";
-import memberReducer from "../members/memberSlice";
+import projectReducer from "../slice/projectslice";
+
 
 export const store = configureStore({
   reducer: {
