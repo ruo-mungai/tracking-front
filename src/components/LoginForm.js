b37c61dfbf8791bb6b729fc9d63995825740a2f3
Yahya <yahyahussein670@gmail.com>
2022-11-02 19:53:57 +0300

    Add the structure and layout of the login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { Button, Error, Input, FormField, Label } from "../styles";
+import { Form, Button, Alert } from 'react-bootstrap'
+let headerPic = require("../Reset password.gif")
 
-function LoginForm({ onLogin }) {
+
+function LoginForm({ onLogin, setShowLogin }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -27,38 +29,54 @@ function LoginForm({ onLogin }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormField>
-        <Label htmlFor="username">Username</Label>
-        <Input
+    <div className="t-main">
+      <div className="headerimage">
+        <img src={headerPic} alt="lock-gif" />
+        </div>
+      <div className="log">
+      <div className='w-100' style={{ maxWidth: "370px" }}>
+      <h2 className="header">Welcome back, Log in to continue</h2>
+        <Form onSubmit={handleSubmit} id='form'>
+        {errors.map((err) => (
+          <Alert variant="danger">{err}</Alert>
+        ))}
+        <Form.Group id="username">
+        <Form.Label>Enter Username*</Form.Label>
+        <Form.Control
           type="text"
-          id="username"
           autoComplete="off"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-        />
-      </FormField>
-      <FormField>
-        <Label htmlFor="password">Password</Label>
-        <Input
+          required
+          />
+        </Form.Group>
+
+      <Form.Group id="password">
+        <Form.Label>Enter Password*</Form.Label>
+        <Form.Control
           type="password"
-          id="password"
-          autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+          required
         />
-      </FormField>
-      <FormField>
-        <Button variant="fill" color="primary" type="submit">
-          {isLoading ? "Loading..." : "Login"}
-        </Button>
-      </FormField>
-      <FormField>
-        {errors.map((err) => (
+      </Form.Group>
+      <br />
+      <Button className='w-100' id="primary-btn" type="submit">{isLoading ? "Loading..." : "Log in"}</Button>
+        {/* {errors.map((err) => (
           <Error key={err}>{err}</Error>
-        ))}
-      </FormField>
-    </form>
+        ))} */}
+      <p className="login-link">
+        Don't have an account? &nbsp;
+        <Button id="secondary" onClick={() => setShowLogin(false)}>
+          Sign Up
+          </Button>
+      </p>
+
+        </Form>
+      </div>
+      </div>
+      </div>
   );
 }
 
